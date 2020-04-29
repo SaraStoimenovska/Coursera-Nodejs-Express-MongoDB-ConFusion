fc6c2fca6283841318b4fc9e1b2e98c050ace14f
Sara Stoimenovska <sara.stoimenovska@yahoo.com>
2020-04-29 11:57:08 +0200

    Passport

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,9 +15,6 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
-
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
@@ -48,23 +49,19 @@ app.use(
 	})
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-	console.log(req.headers);
-	if (!req.session.user) {
+	if (!req.user) {
 		var err = new Error('You are not authenticated!');
 		err.status = 403;
 		return next(err);
 	} else {
-		if (req.session.user === 'authenticated') {
-			next();
-		} else {
-			var err = new Error('You are not authenticated!');
-			err.status = 403;
-			return next(err);
-		}
+		next();
 	}
 }
 
