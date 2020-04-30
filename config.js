ab1de4b6fbe2917234865986f758176afdb32ab8
Sara Stoimenovska <sara.stoimenovska@yahoo.com>
2020-04-30 15:10:17 +0200

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1918180375122700',
+        clientSecret: '81396090d3dfbe45b1f966f434dd9b6f'
+    }
 }
